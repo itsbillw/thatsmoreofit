276a18883b70c55a70ebdcee282395677b79dd31
itsbillw <itsbillw@gmail.com>
2019-10-12 01:29:57 +0200

    switch to parquet data

--- data_reload.py
+++ data_reload.py
@@ -2,12 +2,188 @@ import pandas as pd
 
 
 # local testing
-filename = "data/football_data.csv"
+filename = "data/football_data.parquet"
 
 # python anywhere local file
-# filename = "/home/itsbillw/thatsmoreofit/data/football_data.csv"
+# filename = "/home/itsbillw/thatsmoreofit/data/football_data.parquet"
 
 multi_season_leagues = {
+    "1993-94": {
+        "Premier League": "http://www.football-data.co.uk/mmz4281/9394/E0.csv",
+        "La Liga": "http://www.football-data.co.uk/mmz4281/9394/SP1.csv",
+        "Serie A": "http://www.football-data.co.uk/mmz4281/9394/I1.csv",
+        "Bundesliga": "http://www.football-data.co.uk/mmz4281/9394/D1.csv",
+        "Ligue 1": "http://www.football-data.co.uk/mmz4281/9394/F1.csv",
+        "Eredivisie": "http://www.football-data.co.uk/mmz4281/9394/N1.csv"
+    },
+    "1994-95": {
+        "Premier League": "http://www.football-data.co.uk/mmz4281/9495/E0.csv",
+        "La Liga": "http://www.football-data.co.uk/mmz4281/9495/SP1.csv",
+        "Serie A": "http://www.football-data.co.uk/mmz4281/9495/I1.csv",
+        "Bundesliga": "http://www.football-data.co.uk/mmz4281/9495/D1.csv",
+        "Ligue 1": "http://www.football-data.co.uk/mmz4281/9495/F1.csv",
+        "Eredivisie": "http://www.football-data.co.uk/mmz4281/9495/N1.csv"
+    },
+    "1995-96": {
+        "Premier League": "http://www.football-data.co.uk/mmz4281/9596/E0.csv",
+        "La Liga": "http://www.football-data.co.uk/mmz4281/9596/SP1.csv",
+        "Serie A": "http://www.football-data.co.uk/mmz4281/9596/I1.csv",
+        "Bundesliga": "http://www.football-data.co.uk/mmz4281/9596/D1.csv",
+        "Ligue 1": "http://www.football-data.co.uk/mmz4281/9596/F1.csv",
+        "Eredivisie": "http://www.football-data.co.uk/mmz4281/9596/N1.csv"
+    },
+    "1996-97": {
+        "Premier League": "http://www.football-data.co.uk/mmz4281/9697/E0.csv",
+        "La Liga": "http://www.football-data.co.uk/mmz4281/9697/SP1.csv",
+        "Serie A": "http://www.football-data.co.uk/mmz4281/9697/I1.csv",
+        "Bundesliga": "http://www.football-data.co.uk/mmz4281/9697/D1.csv",
+        "Ligue 1": "http://www.football-data.co.uk/mmz4281/9697/F1.csv",
+        "Eredivisie": "http://www.football-data.co.uk/mmz4281/9697/N1.csv"
+    },
+    "1997-98": {
+        "Premier League": "http://www.football-data.co.uk/mmz4281/9798/E0.csv",
+        "La Liga": "http://www.football-data.co.uk/mmz4281/9798/SP1.csv",
+        "Serie A": "http://www.football-data.co.uk/mmz4281/9798/I1.csv",
+        "Bundesliga": "http://www.football-data.co.uk/mmz4281/9798/D1.csv",
+        "Ligue 1": "http://www.football-data.co.uk/mmz4281/9798/F1.csv",
+        "Eredivisie": "http://www.football-data.co.uk/mmz4281/9798/N1.csv"
+    },
+    "1998-99": {
+        "Premier League": "http://www.football-data.co.uk/mmz4281/9899/E0.csv",
+        "La Liga": "http://www.football-data.co.uk/mmz4281/9899/SP1.csv",
+        "Serie A": "http://www.football-data.co.uk/mmz4281/9899/I1.csv",
+        "Bundesliga": "http://www.football-data.co.uk/mmz4281/9899/D1.csv",
+        "Ligue 1": "http://www.football-data.co.uk/mmz4281/9899/F1.csv",
+        "Eredivisie": "http://www.football-data.co.uk/mmz4281/9899/N1.csv"
+    },
+    "1999-00": {
+        "Premier League": "http://www.football-data.co.uk/mmz4281/9900/E0.csv",
+        "La Liga": "http://www.football-data.co.uk/mmz4281/9900/SP1.csv",
+        "Serie A": "http://www.football-data.co.uk/mmz4281/9900/I1.csv",
+        "Bundesliga": "http://www.football-data.co.uk/mmz4281/9900/D1.csv",
+        "Ligue 1": "http://www.football-data.co.uk/mmz4281/9900/F1.csv",
+        "Eredivisie": "http://www.football-data.co.uk/mmz4281/9900/N1.csv"
+    },
+    "2000-01": {
+        "Premier League": "http://www.football-data.co.uk/mmz4281/0001/E0.csv",
+        "La Liga": "http://www.football-data.co.uk/mmz4281/0001/SP1.csv",
+        "Serie A": "http://www.football-data.co.uk/mmz4281/0001/I1.csv",
+        "Bundesliga": "http://www.football-data.co.uk/mmz4281/0001/D1.csv",
+        "Ligue 1": "http://www.football-data.co.uk/mmz4281/0001/F1.csv",
+        "Eredivisie": "http://www.football-data.co.uk/mmz4281/0001/N1.csv"
+    },
+    "2001-02": {
+        "Premier League": "http://www.football-data.co.uk/mmz4281/0102/E0.csv",
+        "La Liga": "http://www.football-data.co.uk/mmz4281/0102/SP1.csv",
+        "Serie A": "http://www.football-data.co.uk/mmz4281/0102/I1.csv",
+        "Bundesliga": "http://www.football-data.co.uk/mmz4281/0102/D1.csv",
+        "Ligue 1": "http://www.football-data.co.uk/mmz4281/0102/F1.csv",
+        "Eredivisie": "http://www.football-data.co.uk/mmz4281/0102/N1.csv"
+    },
+    "2002-03": {
+        "Premier League": "http://www.football-data.co.uk/mmz4281/0203/E0.csv",
+        "La Liga": "http://www.football-data.co.uk/mmz4281/0203/SP1.csv",
+        "Serie A": "http://www.football-data.co.uk/mmz4281/0203/I1.csv",
+        "Bundesliga": "http://www.football-data.co.uk/mmz4281/0203/D1.csv",
+        "Ligue 1": "http://www.football-data.co.uk/mmz4281/0203/F1.csv",
+        "Eredivisie": "http://www.football-data.co.uk/mmz4281/0203/N1.csv"
+    },
+    "2003-04": {
+        "Premier League": "http://www.football-data.co.uk/mmz4281/0304/E0.csv",
+        "La Liga": "http://www.football-data.co.uk/mmz4281/0304/SP1.csv",
+        "Serie A": "http://www.football-data.co.uk/mmz4281/0304/I1.csv",
+        "Bundesliga": "http://www.football-data.co.uk/mmz4281/0304/D1.csv",
+        "Ligue 1": "http://www.football-data.co.uk/mmz4281/0304/F1.csv",
+        "Eredivisie": "http://www.football-data.co.uk/mmz4281/0304/N1.csv"
+    },
+    "2004-05": {
+        "Premier League": "http://www.football-data.co.uk/mmz4281/0405/E0.csv",
+        "La Liga": "http://www.football-data.co.uk/mmz4281/0405/SP1.csv",
+        "Serie A": "http://www.football-data.co.uk/mmz4281/0405/I1.csv",
+        "Bundesliga": "http://www.football-data.co.uk/mmz4281/0405/D1.csv",
+        "Ligue 1": "http://www.football-data.co.uk/mmz4281/0405/F1.csv",
+        "Eredivisie": "http://www.football-data.co.uk/mmz4281/0405/N1.csv"
+    },
+    "2005-06": {
+        "Premier League": "http://www.football-data.co.uk/mmz4281/0506/E0.csv",
+        "La Liga": "http://www.football-data.co.uk/mmz4281/0506/SP1.csv",
+        "Serie A": "http://www.football-data.co.uk/mmz4281/0506/I1.csv",
+        "Bundesliga": "http://www.football-data.co.uk/mmz4281/0506/D1.csv",
+        "Ligue 1": "http://www.football-data.co.uk/mmz4281/0506/F1.csv",
+        "Eredivisie": "http://www.football-data.co.uk/mmz4281/0506/N1.csv"
+    },
+    "2006-07": {
+        "Premier League": "http://www.football-data.co.uk/mmz4281/0607/E0.csv",
+        "La Liga": "http://www.football-data.co.uk/mmz4281/0607/SP1.csv",
+        "Serie A": "http://www.football-data.co.uk/mmz4281/0607/I1.csv",
+        "Bundesliga": "http://www.football-data.co.uk/mmz4281/0607/D1.csv",
+        "Ligue 1": "http://www.football-data.co.uk/mmz4281/0607/F1.csv",
+        "Eredivisie": "http://www.football-data.co.uk/mmz4281/0607/N1.csv"
+    },
+    "2007-08": {
+        "Premier League": "http://www.football-data.co.uk/mmz4281/0708/E0.csv",
+        "La Liga": "http://www.football-data.co.uk/mmz4281/0708/SP1.csv",
+        "Serie A": "http://www.football-data.co.uk/mmz4281/0708/I1.csv",
+        "Bundesliga": "http://www.football-data.co.uk/mmz4281/0708/D1.csv",
+        "Ligue 1": "http://www.football-data.co.uk/mmz4281/0708/F1.csv",
+        "Eredivisie": "http://www.football-data.co.uk/mmz4281/0708/N1.csv"
+    },
+    "2008-09": {
+        "Premier League": "http://www.football-data.co.uk/mmz4281/0809/E0.csv",
+        "La Liga": "http://www.football-data.co.uk/mmz4281/0809/SP1.csv",
+        "Serie A": "http://www.football-data.co.uk/mmz4281/0809/I1.csv",
+        "Bundesliga": "http://www.football-data.co.uk/mmz4281/0809/D1.csv",
+        "Ligue 1": "http://www.football-data.co.uk/mmz4281/0809/F1.csv",
+        "Eredivisie": "http://www.football-data.co.uk/mmz4281/0809/N1.csv"
+    },
+    "2009-10": {
+        "Premier League": "http://www.football-data.co.uk/mmz4281/0910/E0.csv",
+        "La Liga": "http://www.football-data.co.uk/mmz4281/0910/SP1.csv",
+        "Serie A": "http://www.football-data.co.uk/mmz4281/0910/I1.csv",
+        "Bundesliga": "http://www.football-data.co.uk/mmz4281/0910/D1.csv",
+        "Ligue 1": "http://www.football-data.co.uk/mmz4281/0910/F1.csv",
+        "Eredivisie": "http://www.football-data.co.uk/mmz4281/0910/N1.csv"
+    },
+    "2010-11": {
+        "Premier League": "http://www.football-data.co.uk/mmz4281/1011/E0.csv",
+        "La Liga": "http://www.football-data.co.uk/mmz4281/1011/SP1.csv",
+        "Serie A": "http://www.football-data.co.uk/mmz4281/1011/I1.csv",
+        "Bundesliga": "http://www.football-data.co.uk/mmz4281/1011/D1.csv",
+        "Ligue 1": "http://www.football-data.co.uk/mmz4281/1011/F1.csv",
+        "Eredivisie": "http://www.football-data.co.uk/mmz4281/1011/N1.csv"
+    },
+    "2011-12": {
+        "Premier League": "http://www.football-data.co.uk/mmz4281/1112/E0.csv",
+        "La Liga": "http://www.football-data.co.uk/mmz4281/1112/SP1.csv",
+        "Serie A": "http://www.football-data.co.uk/mmz4281/1112/I1.csv",
+        "Bundesliga": "http://www.football-data.co.uk/mmz4281/1112/D1.csv",
+        "Ligue 1": "http://www.football-data.co.uk/mmz4281/1112/F1.csv",
+        "Eredivisie": "http://www.football-data.co.uk/mmz4281/1112/N1.csv"
+    },
+    "2012-13": {
+        "Premier League": "http://www.football-data.co.uk/mmz4281/1213/E0.csv",
+        "La Liga": "http://www.football-data.co.uk/mmz4281/1213/SP1.csv",
+        "Serie A": "http://www.football-data.co.uk/mmz4281/1213/I1.csv",
+        "Bundesliga": "http://www.football-data.co.uk/mmz4281/1213/D1.csv",
+        "Ligue 1": "http://www.football-data.co.uk/mmz4281/1213/F1.csv",
+        "Eredivisie": "http://www.football-data.co.uk/mmz4281/1213/N1.csv"
+    },
+    "2013-14": {
+        "Premier League": "http://www.football-data.co.uk/mmz4281/1314/E0.csv",
+        "La Liga": "http://www.football-data.co.uk/mmz4281/1314/SP1.csv",
+        "Serie A": "http://www.football-data.co.uk/mmz4281/1314/I1.csv",
+        "Bundesliga": "http://www.football-data.co.uk/mmz4281/1314/D1.csv",
+        "Ligue 1": "http://www.football-data.co.uk/mmz4281/1314/F1.csv",
+        "Eredivisie": "http://www.football-data.co.uk/mmz4281/1314/N1.csv"
+    },
+    "2014-15": {
+        "Premier League": "http://www.football-data.co.uk/mmz4281/1415/E0.csv",
+        "La Liga": "http://www.football-data.co.uk/mmz4281/1415/SP1.csv",
+        "Serie A": "http://www.football-data.co.uk/mmz4281/1415/I1.csv",
+        "Bundesliga": "http://www.football-data.co.uk/mmz4281/1415/D1.csv",
+        "Ligue 1": "http://www.football-data.co.uk/mmz4281/1415/F1.csv",
+        "Eredivisie": "http://www.football-data.co.uk/mmz4281/1415/N1.csv"
+    },
     "2015-16": {
         "Premier League": "http://www.football-data.co.uk/mmz4281/1516/E0.csv",
         "La Liga": "http://www.football-data.co.uk/mmz4281/1516/SP1.csv",
@@ -54,7 +230,7 @@ multi_season_leagues = {
 def rebuild_current_season_data(file=filename):
 
     season = "2019-20"
-    df = pd.read_csv(file, parse_dates=["Date"], dayfirst=True)
+    df = pd.read_parquet(file)
     df = df[df["Season"] != season]
     for league in multi_season_leagues[season]:
         season_data = parse_season_data(
@@ -62,7 +238,7 @@ def rebuild_current_season_data(file=filename):
         season_data["Season"] = season
         season_data["League"] = league
         df = df.append(season_data)
-    df.to_csv(file, index=False)
+    df.to_parquet(file, index=False)
 
 
 def rebuild_all_season_data(file=filename):
@@ -75,7 +251,7 @@ def rebuild_all_season_data(file=filename):
             season_data["Season"] = season
             season_data["League"] = league
             df = df.append(season_data)
-    df.to_csv(file, index=False)
+    df.to_parquet(file, index=False)
 
 
 def parse_season_data(filename):
